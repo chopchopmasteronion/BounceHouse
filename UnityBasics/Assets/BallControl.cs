fbcb3e5b8de45682bd7e4b5895066b1bce5da324
flex <flexedoyster@hotmail.com>
2014-08-18 21:52:57 -0400

    Adding UnityBasics
    First successful unity work I have done, feel free to tool around, also
    using this as an excuse to play with git.

---
+++ UnityBasics/Assets/BallControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControl : MonoBehaviour {
+
+	public float rotationSpeed = 100;
+
+	void Update () 
+	{
+		float rotation = Input.GetAxis ("Horizontal") * rotationSpeed;
+		rotation *= Time.deltaTime; 
+		rigidbody.AddRelativeTorque (Vector3.back * rotation);
+
+	}
+
+}
