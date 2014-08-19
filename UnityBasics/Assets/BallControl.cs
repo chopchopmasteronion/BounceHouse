0c9dffd221dcadb5f761fa0be8d07141c8622fdf
flex <flexedoyster@hotmail.com>
2014-08-18 23:20:54 -0400

    Jump and Camera
    Added a jump ability to the ball as well as made the camera follow the
    ball.

--- UnityBasics/Assets/BallControl.cs
+++ UnityBasics/Assets/BallControl.cs
@@ -4,13 +4,23 @@ using System.Collections;
 public class BallControl : MonoBehaviour {
 
 	public float rotationSpeed = 100;
-
+	public float jumpHeight = 8;
+	private bool isFalling = false;
 	void Update () 
 	{
+		//Handle ball rotation.
 		float rotation = Input.GetAxis ("Horizontal") * rotationSpeed;
 		rotation *= Time.deltaTime; 
 		rigidbody.AddRelativeTorque (Vector3.back * rotation);
 
+		if (Input.GetKeyDown (KeyCode.Space) && isFalling == false) 
+		{
+			rigidbody.velocity = new Vector3(rigidbody.velocity.x, jumpHeight);
+		}
+		isFalling = true;
 	}
-
+	public void OnCollisionStay()
+	{
+		isFalling = false;
+	}
 }
