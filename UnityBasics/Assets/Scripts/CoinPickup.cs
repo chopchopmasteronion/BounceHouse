ef2abe743a178a5b190e534c67be945148dfecd5
flex <flexedoyster@hotmail.com>
2014-08-19 21:18:14 -0400

    Metric Crapton
    Lights, Coins, Death, etc

---
+++ UnityBasics/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinPickup : MonoBehaviour {
+	
+	public Transform coinEffect;
+	private Object effect;
+
+	void OnTriggerEnter (Collider info) 
+	{
+	if (info.tag == "Player")
+		{
+			effect = Instantiate(coinEffect, transform.position, transform.rotation);
+
+			Destroy(gameObject);
+
+			Destroy((effect as Transform).gameObject, 3);
+		}
+	}
+}
