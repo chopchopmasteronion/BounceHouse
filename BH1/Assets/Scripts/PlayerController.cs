e85c743f157d420e98146c8753ca11b7191b8a52
flex <flexedoyster@hotmail.com>
2014-08-20 22:22:00 -0400

    BounceHouse1
    Using some free unity samples I have started to put together a debug
    level.

---
+++ BH1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpHeight;
+	public bool isFalling;
+
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		rigidbody2D.velocity = new Vector2 (moveHorizontal * moveSpeed, rigidbody2D.velocity.y);
+
+		if (Input.GetKeyDown (KeyCode.Space) && isFalling == false) 
+		{
+			rigidbody2D.velocity = new Vector2(rigidbody.velocity.x, jumpHeight);
+		}
+		isFalling = true;
+	}
+
+	public void OnCollisionStay()
+	{
+		isFalling = false;
+	}
+}
